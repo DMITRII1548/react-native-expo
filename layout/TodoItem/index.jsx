b37a59fab3d3647f909ebc2c347457e2a7bd6f60
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-12-23 22:50:41 +0600

    Stylize TodoItem

--- layout/TodoItem/index.jsx
+++ layout/TodoItem/index.jsx
@@ -1,5 +1,7 @@
 import StyledText from "@/components/StyledText"
+import { COLORS } from "@/constants/ui"
 import React from "react"
+import { StyleSheet, View } from "react-native"
 
 type TodoItemProps = {
     title: string,
@@ -7,11 +9,29 @@ type TodoItemProps = {
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ title, isCompleted }) => {
-    return <StyledText>{title}</StyledText>
+    return (
+        <View style={styles.container}>
+            <StyledText
+                style={[{
+                    textDecoration: isCompleted ? 'line-through' : 'none'
+                }]}
+            >
+                {title}
+            </StyledText>
+        </View>
+    )
 }
 
 export default TodoItem
 
-const styles = {
-    
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'space-between',
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: COLORS.SECONDARY_BACKGROUND
+    }
+})
