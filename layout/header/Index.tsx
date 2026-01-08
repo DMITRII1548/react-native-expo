f3d5a4e30cba6303035336b22c19a5c84ee72b69
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2026-01-08 22:36:39 +0600

    Add current date auto generation

--- layout/header/Index.tsx
+++ layout/header/Index.tsx
@@ -1,5 +1,6 @@
 import StyledText from "@/components/StyledText"
 import { COLORS } from "@/constants/ui"
+import { getFullFormatedDate } from "@/helpers/date"
 import React from "react"
 import { View, Text, StyleSheet } from "react-native"
 
@@ -9,11 +10,12 @@ type HeaderProps = {
 }
 
 const Header: React.FC<HeaderProps> = ({ totalTodos, completedTodos }) => {
+    const formattedDateNow = getFullFormatedDate(new Date())
     return (
         <View style={styles.container}>
             <View style={styles.headerMainContent}>
                 <StyledText>TO DO LIST</StyledText>
-                <StyledText>December 19, 2025</StyledText>
+                <StyledText>{ formattedDateNow }</StyledText>
             </View>
             <StyledText>Completed: {completedTodos} / {totalTodos}</StyledText>
         </View>
